a8dffa763b753ad95fbde5a87af3d36283686580
publiusfederalist <publiusfederalist@tutanota.com>
2021-12-10 17:36:48 +0000

    Fixed old relic detecting hashes versus magnet links

--- include/hresolver.js
+++ include/hresolver.js
@@ -1,13 +1,12 @@
 const doh = require('dohjs');
-const resolver = new doh.DohResolver('https://easyhandshake.com:8053/dns-query');
+const resolver = new doh.DohResolver('https://query.hdns.io/dns-query');
 
 let hresolver = async function(name) {
   let response = await resolver.query(name,'TXT');
   if(response.answers.length>0) {
     let returned=null;
     response.answers.forEach(ans=>{
-      if(ans.data.toString().length==40)
-        returned = ans.data.toString();
+      returned = ans.data.toString();
     });
     return (returned?returned:null);
   } 
